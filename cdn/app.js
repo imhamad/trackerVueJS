4f50ade220adc4b6bbe737c874912a235ffe16fe
Hamad <hamad.coder@gmail.com>
2022-02-23 19:21:43 +0500

    v-bind the image

--- cdn/app.js
+++ cdn/app.js
@@ -1,4 +1,11 @@
 const app = Vue.createApp({
-    template: '<h1>Howdy</h1>',
+    data(){
+        return{
+            firstName: 'Hamad',
+            lastName: 'K',
+            email: 'hamad@netchainmedia.com',
+            picture: 'https://randomuser.me/api/portraits/men/11.jpg'
+        }
+    }
 })
 app.mount('#render')
