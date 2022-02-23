2704999c39691611a256db17f2f7745eabea612c
Hamad <hamad.coder@gmail.com>
2022-02-23 19:49:13 +0500

    Updates Performed:: savepoint ~ Hamad

--- cdn/app.js
+++ cdn/app.js
@@ -1,11 +1,16 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            firstName: 'Hamad',
+    data() {
+        return {
+            firstName: 'John',
             lastName: 'K',
             email: 'hamad@netchainmedia.com',
             gender: 'female',
             picture: 'https://randomuser.me/api/portraits/men/11.jpg'
+        }
+    },
+    methods: {
+        getUser(){
+            console.log(this.firstName)
         }
     }
 })
