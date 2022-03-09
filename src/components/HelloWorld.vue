b25b444e2c8dcfae87d20f5626e1a5f3230957a6
Hamad <hamad.coder@gmail.com>
2022-03-09 13:26:55 +0500

    Updates Performed:: savepoint ~ Hamad

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,18 +2,17 @@
 defineProps({
   msg: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+Hey welcome to your project
+
     </h3>
   </div>
 </template>
